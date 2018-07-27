275c20635a231c583b93653cb060f6a15f3d00db
HP <HP@DESKTOP-5H7JUEO>
2018-07-27 14:18:15 +0530

    Intitial upload

---
+++ Practical/src/main/java/com/java/demo/Practical/App.java
@@ -0,0 +1,13 @@
+package com.java.demo.Practical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
