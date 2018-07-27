7c460c5ff81c6018012b03ad6667ec0e8c7cd7f3
HP <HP@DESKTOP-5H7JUEO>
2018-07-27 14:26:52 +0530

    Simple changes

---
+++ Practical/src/main/java/com/java/demo/Practical/UploadToGit.java
@@ -0,0 +1,12 @@
+package com.java.demo.Practical;
+
+public class UploadToGit {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Hello Welcome to git");
+		
+		System.out.println("Hello world");
+	}
+
+}
