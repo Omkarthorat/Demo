a6ef798f89d484f7f58a854a30d423c2e7f38a70
HP <HP@DESKTOP-5H7JUEO>
2018-07-31 11:47:09 +0530

    Reverse String

---
+++ demo/src/main/java/com/java/demo/ReverseString.java
@@ -0,0 +1,13 @@
+package com.java.demo;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sb= new StringBuilder("My name is omkar");
+		
+		System.out.println(sb.reverse());
+
+	}
+
+}
